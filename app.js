9bfb399f184216adc30cf9f77af86181eed8a73b
Kaushik Kumar Barman <kaushik.kb4@gmail.com>
2020-11-14 23:25:32 +0530

    Added basic functionality without using any storage

---
+++ app.js
@@ -0,0 +1,41 @@
+let tasks;
+
+loadEventListners();
+
+function loadEventListners() {
+    document.querySelector('#add-task-form').addEventListener('submit', addNewTask);
+    document.querySelector('.collection').addEventListener('click', deleteTask);
+}
+
+function addNewTask(e) {
+    let new_task = document.querySelector('#new-task').value;
+    if(new_task === '') {
+        alert('Task cannot be empty');
+    }
+    else {
+        createTaskItem(new_task);
+        document.querySelector('#new-task').innerHTML = '';
+        console.log(document.querySelector('#new-task').value);
+    }
+}
+
+function createTaskItem(new_task) {
+    let task = document.createElement('li');
+    task.className = 'collection-item';
+    task.appendChild(document.createTextNode(new_task));
+
+    let deleteBtn = document.createElement('a');
+    deleteBtn.setAttribute('href', '#');
+    deleteBtn.className = 'secondary-content';
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    task.appendChild(deleteBtn);
+
+    document.querySelector('.collection').appendChild(task);
+}
+
+function deleteTask(e) {
+    if(e.target.className == 'secondary-content') {
+        e.target.parentElement.remove();
+    }
+}
